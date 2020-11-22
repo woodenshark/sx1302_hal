7be64cc9e53d2648d3c956812c5f3f6de837f8e4
Aleksandr Gusarov <alexandergusarov@gmail.com>
2020-11-23 02:28:08 +0300

    US915 support, gps disable

--- start.sh
+++ start.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-echo $(./chip_id)
-sed -i "s_AA555A0000000000_$(./chip_id | grep "EUI:" | cut -d' ' -f4 | cut -c3-)_g" global_conf.json.sx1250.EU868
+CONFIG_NAME_PREFIX=global_conf.json.sx1250
+LORA_REGION=${APP_ENV_LORA_REGION}
 
-./lora_pkt_fwd -c global_conf.json.sx1250.EU868
+case "$LORA_REGION" in
+
+"EU868" )
+chip_id_shift=0
+;;
+
+"US915" )
+chip_id_shift=1
+;;
+
+* )
+LORA_REGION=EU868
+chip_id_shift=0
+;;
+
+esac
+
+chip_id=$(./chip_id | grep "EUI:" | cut -d' ' -f4)
+echo "SX1302 chip ID is : ${chip_id}"
+
+gw_id_decimal=$((chip_id + 0x0000001000000000 * chip_id_shift))
+gw_id=$(printf "%x\n" ${gw_id_decimal})
+echo "Gateway ID is : 0x${gw_id}"
+
+sed -i "s_AA555A0000000000_${gw_id}_g" ${CONFIG_NAME_PREFIX}.${LORA_REGION}
+
+./lora_pkt_fwd -c ${CONFIG_NAME_PREFIX}.${LORA_REGION}
 
 #balena-idle --interval 60 --message "Container idle for debug..."
